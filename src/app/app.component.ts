8118cc2d90e0d69f87b5719ff4d1ff6b86436654
Miroslav Jonaš <meeroslav@yahoo.com>
2018-01-25 14:34:56 +0100

    Add project files and set up the workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Game } from './game/game';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('gameCanvas') canvasRef: ElementRef;
+  private game: Game;
+
+  ngOnInit() {
+    const ctx = this.canvasRef.nativeElement.getContext('2d');
+    this.game = new Game(ctx);
+  }
+
+  toggleGame() {
+    this.game.toggleGame();
+  }
 }
---
+++ src/app/game/game.ts
@@ -0,0 +1,8 @@
+export class Game {
+  constructor(private ctx: HTMLCanvasElement) {
+  }
+
+  toggleGame() {
+    alert('Im not implemented yet');
+  }
+}
